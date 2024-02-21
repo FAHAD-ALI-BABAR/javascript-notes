10fd0218b5e64649ca7a8d55901a056ca3c82cfb
Fahad Ali Babar <127986861+FAHAD-ALI-BABAR@users.noreply.github.com>
2024-02-21 20:39:24 +0500

    container

---
+++ num&maths.js
@@ -0,0 +1,25 @@
+let a=10;
+let b="10";
+console.log(a==b);
+console.log(a===b);//this will only check the value
+// // console.log(a===b);//this wil also check the datatype of variable
+// // //
+// // let num=new String(50);
+// // console.log(num);
+// // let num1= String(num);
+// // console.log(typeof num1);
+
+// let num=100;
+// console.log(num);
+// console.log(num.toString());
+// console.log(num.toFixed(3));//this means how many numbers appears after decimal....mostly used to show price of something
+
+//let num1=104.7899;
+//console.log(num1.toPrecision(3));//this will give the most precise value/round-of value ...
+// *******************________________________________________MATHS______________________//
+console.log(Math);
+console.log(Math.abs(-4));//this abs will change the negative sign to positive
+console.log(Math.round(4.6));//this will round off value and remove decimal
+console.log(Math.round(4.3))
+console.log(Math.ceil(4.3))//this will take the upper value and round off it irrespective of the number after decimal
+console.log(Math.floor(3.9))//this will take lowest value irrespective of the value after decimal
