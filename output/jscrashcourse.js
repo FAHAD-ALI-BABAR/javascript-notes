b2ed388e4dc1faab76ede6cbe12826d54cbf6e91
Fahad Ali Babar <127986861+FAHAD-ALI-BABAR@users.noreply.github.com>
2024-03-21 22:15:16 +0500

    crash

---
+++ output/jscrashcourse.js
@@ -0,0 +1,10 @@
+// let a=[1,2,3,4];
+// let b=a;
+// b.pop();
+// console.log(b);
+// console.log(a);//this is the difference in non-primitve values
+let a=123;
+let b=a;
+b=321;
+console.log(b);
+console.log(a);
