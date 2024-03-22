8c58257222957f5dd820573750e70f285b45aea4
Fahad Ali Babar <127986861+FAHAD-ALI-BABAR@users.noreply.github.com>
2024-03-22 14:51:05 +0500

    jscrashcourse

--- output/jscrashcourse.js
+++ output/jscrashcourse.js
@@ -3,31 +3,45 @@
 // b.pop();
 // console.log(b);
 // console.log(a);//this is the difference in non-primitve values
-let a=123;
+// let a=123;
+// let b=a;
+// b=321;
+// console.log(b);
+// console.log(a);
+
+// //FUNCTION JB AAp apna code foran nhi chlana chahty future mn chlana chahty ho.
+// //jb ap apna code baar baar reuse krna chahty ho
+// function fahad(a=10){
+//     console.log(a);
+// }
+// console.log(fahad());
+// //argument are the values we give in function declaration
+// //parameters are the values we accept in function defination
+
+
+// let z=[1,2,3,4,5,6,7];
+// z.push(10);
+// console.log(z);
+// z.pop();
+// console.log(z);
+// z.shift();
+// console.log(z);
+// z.unshift(20)//it is used to add value at the start of array
+// console.log(z);
+
+// z.splice(3,3);//first parameter shows which index to remove and second parameter show from that index how many next value
+// console.log(z);
+
+// let y=12;
+// var x=12;
+let a=[1,2,3,4,5];
 let b=a;
-b=321;
+b.pop();
 console.log(b);
-console.log(a);
-
-//FUNCTION JB AAp apna code foran nhi chlana chahty future mn chlana chahty ho.
-//jb ap apna code baar baar reuse krna chahty ho
-function fahad(a=10){
-    console.log(a);
-}
-console.log(fahad());
-//argument are the values we give in function declaration
-//parameters are the values we accept in function defination
-
-
-let z=[1,2,3,4,5,6,7];
-z.push(10);
-console.log(z);
-z.pop();
-console.log(z);
-z.shift();
-console.log(z);
-z.unshift(20)//it is used to add value at the start of array
-console.log(z);
-
-z.splice(3,3);//first parameter shows which index to remove and second parameter show from that index how many next value
-console.log(z);
+console.log(a);//now the result would be change in b and akso in a to stop this we use spread operator
+//now this will pop only from t not from r
+let r=[2,3,4,5,6,7,8]
+let t=[...r];
+t.pop();
+console.log(t);
+console.log(r);
