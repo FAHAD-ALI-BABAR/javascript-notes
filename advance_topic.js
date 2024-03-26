8e30040c93c836e8cc53b3083feffcb2df8b50ad
Fahad Ali Babar <127986861+FAHAD-ALI-BABAR@users.noreply.github.com>
2024-03-26 23:32:24 +0500

    advancejstopics

--- advance_topic.js
+++ advance_topic.js
@@ -29,15 +29,50 @@
 // console.table(cat1);
 //we can also accept parameters
 
-function pet(color){
-    this.name="arlo";
-    this.age=2,
-    this.breed="persian",
-    this.color=color;
+// function pet(color){
+//     this.name="arlo";
+//     this.age=2,
+//     this.breed="persian",
+//     this.color=color;
+//     }
+//     let cat1=new pet("red");
+//     let cat2=new pet("green");
+//     let cat3=new pet("yellow");
+//     console.log(cat1);
+//     console.log(cat2);
+//     console.log(cat3);
+
+//function prototype
+
+// let dog={
+//     canfly:false,
+//     cantalk:false,
+//     canbark:true,
+//     canrun:true,
+// }
+// let mydog={
+//     whitecolor:true,
+//     caneat:true,
+//     name:"arlo",
+//     age:19,
+// }
+// // mydog.__protype__=dog;//prototype is used to inherit the properties
+// // console.log(mydog);
+// Object.keys(mydog);
+// console.log(Object.keys(mydog));
+// console.log(Object.values(mydog));
+// console.log(Object.entries(mydog));
+
+//this keyword
+//  function name(){
+//     console.log(this);
+//  }
+//  name();
+
+let pet={
+    name:"arlo",
+    qualities:function(){ //now thos is method because the function inside an object is caallled a method
+        console.log(this);
     }
-    let cat1=new pet("red");
-    let cat2=new pet("green");
-    let cat3=new pet("yellow");
-    console.log(cat1);
-    console.log(cat2);
-    console.log(cat3);
+}
+pet.qualities();//
